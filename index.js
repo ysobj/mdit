7d5cab6b777d78f768419af8f83cad20e5e1aba7
ysobj <litailang+ysobj@gmail.com>
2015-10-30 13:12:16 +0900

    onclick example

--- index.js
+++ index.js
@@ -3,5 +3,9 @@
 var remote = require('remote');
 var fs = require('fs');
 fs.readFile('./package.json', 'utf-8', function(err,text){
-  document.write(text);
+  //document.write(text);
 });
+
+function test(){
+  alert("Hi!");
+}
