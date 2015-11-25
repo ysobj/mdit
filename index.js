e9d79cf6c17a7bb3458325ed583e11c3de696ddc
ysobj <litailang+ysobj@gmail.com>
2015-11-25 13:19:15 +0900

    20151125

--- index.js
+++ index.js
@@ -3,15 +3,8 @@
 var remote = require('remote');
 var fs = require('fs');
 var marked = require('marked');
-var x =require('highlight');
-console.log("check", x);
-//marked.setOptions({
-//  highlight: function (code) {
-//               return require('highlightjs').highlightAuto(code).value;
-//             }
-//});
 fs.readFile('./README.md', 'utf-8', function(err,text){
-  document.write(marked(text));
+//  document.write(marked(text));
 });
 
 function test(){
