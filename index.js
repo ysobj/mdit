195d26fdbee7f8636e926ae2a3f21dd00ea387ae
ysobj <litailang+ysobj@gmail.com>
2015-11-26 13:13:01 +0900

    20151126

--- index.js
+++ index.js
@@ -3,10 +3,21 @@
 var remote = require('remote');
 var fs = require('fs');
 var marked = require('marked');
+var x = document.querySelector(".editor");
+var y = document.querySelector(".preview");
+var z = document.querySelector("textarea");
 fs.readFile('./README.md', 'utf-8', function(err,text){
 //  document.write(marked(text));
+  z.value = text;
+  y.innerHTML = marked(text);
 });
-
-function test(){
-  alert("Hi!");
-}
+x.addEventListener('click',function(){
+  console.log('x');
+  x.style.display="none";
+  y.style.display="block";
+});
+y.addEventListener('click',function(){
+  console.log('x');
+  y.style.display="none";
+  x.style.display="block";
+});
