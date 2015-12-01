8a6ba38da6d82ab3521d06efe1393496e369bff5
ysobj <litailang+ysobj@gmail.com>
2015-12-01 13:04:08 +0900

    20151201

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ var fs = require('fs');
 var marked = require('marked');
 var editor = document.querySelector(".editor");
 var preview = document.querySelector(".preview");
+var textarea = document.querySelector("textarea");
+var readFile = function(path){
+  fs.readFile(path, 'utf-8', function(err,text){
+    textarea.value = text;
+    preview.innerHTML = marked(text);
+    editor.style.display="none";
+  });
+};
 fs.readFile('./README.md', 'utf-8', function(err,text){
 //  document.write(marked(text));
-  document.querySelector("textarea").value = text;
+  textarea.value = text;
   preview.innerHTML = marked(text);
   editor.style.display="none";
 });
 fs.readdir('.',function(err,files){
-  console.log(files);
   var li = '';
   files.forEach(function(file){
     if(file.charAt(0) !== '.'){
@@ -24,10 +31,14 @@ fs.readdir('.',function(err,files){
   });
   var t = document.querySelector('.fileList');
   t.innerHTML = li;
+  t.addEventListener('click',function(e){
+    readFile(e.target.innerText);
+  });
 });
 document.querySelector(".previewTab").addEventListener('click',function(){
   editor.style.display="none";
   preview.style.display="block";
+  preview.innerHTML = marked(textarea.value);
 });
 document.querySelector(".editorTab").addEventListener('click',function(){
   preview.style.display="none";
